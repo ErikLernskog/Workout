153e0dd38ec253f1181773ae2449b84b19510b1a
Erik Lernskog <funbo@hotmail.com>
2016-03-14 16:30:54 +0100

    Set the views values in the fragments onStart method

--- app/src/main/java/com/lernskog/erik/workout/WorkoutDetailFragment.java
+++ app/src/main/java/com/lernskog/erik/workout/WorkoutDetailFragment.java
@@ -2,10 +2,11 @@ package com.lernskog.erik.workout;
 
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class WorkoutDetailFragment extends Fragment {
@@ -21,6 +22,18 @@ public class WorkoutDetailFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
+    }
+
+    @Override public void onStart() {
+        super.onStart();
+        View view = getView();
+        if (view != null) {
+            TextView title = (TextView) view.findViewById(R.id.textTitle);
+            Workout workout = Workout.workouts[(int) workoutId];
+            title.setText(workout.getName());
+            TextView description = (TextView) view.findViewById(R.id.textDescription);
+            description.setText(workout.getDescription());
+        }
     }
 
     public void setWorkout(long id) {
